cadf3278a93c272b16b8bda84ea748e4a0725a00
chandan-1427 <chandandakka@gmail.com>
2026-06-18 01:02:24 +0530

    fix: use light white for footer hover

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -8,7 +8,7 @@ export const Footer = () => {
   return (
     <footer className="w-full py-16 pb-24 md:pb-16 flex flex-col items-center gap-2">
       <button type="button" onClick={handleClick} className="group cursor-pointer">
-        <h6 className="text-6xl md:text-8xl lg:text-9xl tracking-wide text-white/20 transition-all duration-500 group-hover:text-white/80 active:text-white/80 select-none">
+        <h6 className="text-6xl md:text-8xl lg:text-9xl tracking-wide text-white/20 transition-all duration-500 group-hover:text-white/60 active:text-white/80 select-none">
           Chandan
         </h6>
       </button>
