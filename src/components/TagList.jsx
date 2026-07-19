3ec45fe7f5baa903d3c3a016e992ceb62383ea9e
chandan-1427 <chandandakka@gmail.com>
2026-07-19 21:20:24 +0530

    chore: remove broder from taglist component

--- src/components/TagList.jsx
+++ src/components/TagList.jsx
@@ -6,7 +6,7 @@ export default function TagList({ tags, className = "" }) {
       {tags.map((tag) => (
         <span
           key={tag}
-          className="rounded-md border border-white/[0.08] bg-white/[0.06] px-3 py-1.5 text-xs tracking-wide text-white/70 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.08)]"
+          className="rounded-md bg-white/[0.06] px-3 py-1.5 text-xs tracking-wide text-white/70 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.08)]"
         >
           {tag}
         </span>
